8574cc7c6fbc3f794ffc5bcb5bb6d1d28268c711
Andrii Nyvchyk <andriy.nuvchuk@gmail.com>
2021-08-20 18:35:07 +0300

    1

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import "reflect-metadata";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { RecipesModule } from './users/recipes.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: 'schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'nest',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
